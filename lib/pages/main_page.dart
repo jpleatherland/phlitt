d6884c82cf5350613cff4fca6446062734513ad5
Jack Leatherland <jpl92@live.co.uk>
2024-03-04 16:05:36 +0000

    request titles rendering under request group

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -14,23 +14,43 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   String selectedCollection = '';
   Iterable collection = [];
+  List requestGroups = [];
+
+  Widget renderList(listToRender) {
+    return ListView.builder(
+      itemCount: listToRender.length,
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return TextButton(
+          child: Text(listToRender[index]['requestName']),
+          onPressed: () => print('Pressed'),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     selectedCollection = widget.collectionName;
-    collection = widget.collection.where(((element) => element == 'requestGroups'));
+    collection = widget.collection;
+    requestGroups = widget.collection.toList()[0]['requestGroups'];
     return Row(
+      mainAxisSize: MainAxisSize.min,
       children: [
         Expanded(
-            flex: 1,
-            child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: collection.length,
-                itemBuilder: (context, i) {
-                  return ExpansionTile(
-                      title: Text(collection.elementAt(i).toString()));
-                })),
+          flex: 1,
+          child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: requestGroups.length,
+              itemBuilder: (context, i) {
+                return ExpansionTile(
+                  title: Text(requestGroups[i]['requestGroupName']),
+                  children: [renderList(requestGroups[i]['requests'])],
+                );
+              }),
+        ),
         // NavigationRail(destinations: [
         //   NavigationRailDestination(icon: Icon(Icons.home), label: Text('Hi')),
         //   NavigationRailDestination(icon: Icon(Icons.home), label: Text('Hi1')),
