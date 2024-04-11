12f08ec27861321dad9812732ec84dccfefdd535
Jack Leatherland <jack.leatherland@qbe.com>
2024-04-11 16:41:05 +0100

    web dev

--- lib/utils/collections_manager.dart
+++ lib/utils/collections_manager.dart
@@ -1,43 +1,62 @@
 import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
 
 import 'dart:io';
 import 'dart:convert';
 
-import './example_collection.dart';
+import './example_collection.dart' as exc;
 
 class CollectionsManager {  
+
+  static const url = 'http://0.0.0.0:8000';
   
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
-  }
+  // Future<String> get _localPath async {
+  //   final directory = await getApplicationDocumentsDirectory();
+  //   return directory.path;
+  // }
 
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    final bool exists = await File('$path/collections.json').exists();
-    if (exists) {
-      return File('$path/collections.json');
-    } else {
-      File file = File('$path/collections.json');
-      file.writeAsString(collectionTemplate);
-      return File('$path/collections.json');
+  // local file
+  // Future<File> get _localFile async {
+  //   final path = await _localPath;
+  //   final bool exists = await File('$path/collections.json').exists();
+  //   if (exists) {
+  //     return File('$path/collections.json');
+  //   } else {
+  //     File file = File('$path/collections.json');
+  //     file.writeAsString(collectionTemplate);
+  //     return File('$path/collections.json');
+  //   }
+  // }
+
+  Future<Object> get _getServerFile async {
+    final collectionsFile = await http.get(Uri.parse(url));
+    if (collectionsFile.statusCode == 404){
+      writeCollections(exc.collectionTemplate as Map);
     }
+    return collectionsFile;
   }
+  // local file
+  // Future<Map<String, dynamic>> readCollectionsFile() async {
+  //   try {
+  //     final file = await _localFile;
+  //     final contents = await file.readAsString();
+  //     final collections = jsonDecode(contents) as Map<String, dynamic>;
+  //     return collections;
+  //   } catch (error) {
+  //     return {};
+  //   }
+  // }
 
-  Future<Map<String, dynamic>> readCollectionsFile() async {
+    Future<Map<String, dynamic>> readCollectionsFile() async {
     try {
-      final file = await _localFile;
-      final contents = await file.readAsString();
-      final collections = jsonDecode(contents) as Map<String, dynamic>;
-      return collections;
+      final file = await _getServerFile;
+      return file as Map<String, dynamic>;
     } catch (error) {
       return {};
     }
   }
 
-  Future<File> writeCollections(Map collection) async {
-    final file = await _localFile;
-
-    return file.writeAsString('$collection');
+  Future<void> writeCollections(Map collection) async {
+    http.post(Uri.parse(url), body:{Object: collection});
   }
 }
