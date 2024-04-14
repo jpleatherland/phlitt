325d2ade962e75ac5c96a92070c21afc3e20fda2
Jack Leatherland <jpl92@live.co.uk>
2024-04-14 13:44:53 +0100

    import dynamic tab bar, start on main area

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dynamic_tabbar/dynamic_tabbar.dart';
 import 'package:qapic/widgets/render_request_groups.dart' as rrq;
 
 class MainPage extends StatefulWidget {
@@ -16,37 +17,38 @@ class _MainPageState extends State<MainPage> {
   String selectedCollection = '';
   Iterable collection = [];
   List requestGroups = [];
+  int openRequests = 0;
 
-  Future<String?> _dialogBuilder(BuildContext context) {
-    return showDialog<String>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Title'),
-            content: Text('Replace with input field'),
-            actions: [
-              TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: Theme.of(context).textTheme.labelLarge,
-                ),
-                child: const Text('Disable'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: Theme.of(context).textTheme.labelLarge,
-                ),
-                child: const Text('Enable'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        });
-  }
+  // Future<String?> _dialogBuilder(BuildContext context) {
+  //   return showDialog<String>(
+  //       context: context,
+  //       builder: (BuildContext context) {
+  //         return AlertDialog(
+  //           title: Text('Title'),
+  //           content: Text('Replace with input field'),
+  //           actions: [
+  //             TextButton(
+  //               style: TextButton.styleFrom(
+  //                 textStyle: Theme.of(context).textTheme.labelLarge,
+  //               ),
+  //               child: const Text('Disable'),
+  //               onPressed: () {
+  //                 Navigator.of(context).pop();
+  //               },
+  //             ),
+  //             TextButton(
+  //               style: TextButton.styleFrom(
+  //                 textStyle: Theme.of(context).textTheme.labelLarge,
+  //               ),
+  //               child: const Text('Enable'),
+  //               onPressed: () {
+  //                 Navigator.of(context).pop();
+  //               },
+  //             ),
+  //           ],
+  //         );
+  //       });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -58,13 +60,39 @@ class _MainPageState extends State<MainPage> {
       children: [
         Expanded(
           flex: 1,
-          child: Column(children: [rrq.RenderRequestGroups(requestGroups: requestGroups)]),
+          child: Column(children: [
+            rrq.RenderRequestGroups(
+                requestGroups: requestGroups,
+                parentListName: 'requestGroupName')
+          ]),
         ),
-        const Expanded(
+        Expanded(
             flex: 5,
-            child: Center(
-                child: Text('request and response here',
-                    style: TextStyle(fontSize: 50))))
+            child: DynamicTabBarWidget(
+              onTabControllerUpdated: (controller){},
+              dynamicTabs: <TabData>[
+              TabData(
+                index: 0,
+                title: const Tab(
+                  child: Text('Request 1'),
+                ),
+                content: const Center(child: Text('Content for request 1')),
+              ),
+              TabData(
+                index: 1,
+                title: const Tab(
+                  child: Text('Request 2'),
+                ),
+                content: const Center(child: Text('Content for request 2')),
+              ),
+               TabData(
+                index: 2,
+                title: const Tab(
+                  child: Text('Request 3'),
+                ),
+                content: const Center(child: Text('Content for request 3')),
+              ),
+            ])),
       ],
     );
   }
