ceb5050cad6d5788ff6293f2e946ee4c9f1e12ae
Jack Leatherland <jpl92@live.co.uk>
2024-04-21 22:12:47 +0100

    change approach to query params and path vars

--- lib/widgets/render_request_query.dart
+++ lib/widgets/render_request_query.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:qapic/model/collections_model.dart';
-import 'package:flutter_editable_table/flutter_editable_table.dart';
 
 class RenderRequestQuery extends StatelessWidget {
   final RequestQuery requestQuery;
@@ -8,77 +7,15 @@ class RenderRequestQuery extends StatelessWidget {
 
   const RenderRequestQuery({super.key, required this.requestQuery, required this.context});
 
-
+  void updateRequestQuery(){
+  }
 
   Widget renderQueryOptions() {
-    final queryTableData = {
-    "column_count": 2,
-    "row_count": requestQuery.queryParams.length,
-    "addable": true,
-    "removeable": true,
-    "caption": {
-      "main_caption": {
-        "title": "Query Parameters",
-        "editable": false
-      }
-    },
-    "columns": [
-      {
-        "primary_key": true,
-        "name": "Query Parameter",
-        "type": "String",
-        "editable": true,
-        "display": true,
-        "style": {
-          "font_weight": "bold",
-          "font_size": 14.0,
-          "font_color": "#000000",
-          "background_color": Theme.of(context).colorScheme.secondary,
-          "horizontal_alignment": "center",
-          "vertical_alignment": "center",
-          "text_align": "center"
-        }
-      },
-      {
-        "name": "Value",
-        "type": "String",
-        "editable": true
-      }
-    ],
-    "rows": requestQuery.queryParams.entries
-  };
-    return EditableTable(
-          key: Key('nice key'),
-          data: queryTableData,
-          readOnly: false,
-          tablePadding: EdgeInsets.all(8.0),
-          captionBorder: Border(
-            top: BorderSide(color: Color(0xFF999999)),
-            left: BorderSide(color: Color(0xFF999999)),
-            right: BorderSide(color: Color(0xFF999999)),
-          ),
-          headerBorder: Border.all(color: Color(0xFF999999)),
-          rowBorder: Border.all(color: Color(0xFF999999)),
-          footerBorder: Border.all(color: Color(0xFF999999)),
-          removeRowIcon: Icon(
-            Icons.remove_circle_outline,
-            size: 24.0,
-            color: Colors.redAccent,
-          ),
-          addRowIcon: Icon(
-            Icons.add_circle_outline,
-            size: 24.0,
-            color: Colors.white,
-          ),
-          addRowIconContainerBackgroundColor: Colors.blueAccent,
-          formFieldAutoValidateMode: AutovalidateMode.always,
-          onRowRemoved: (row) {
-            print('row removed: ${row.toString()}');
-          },
-          onRowAdded: () {
-            print('row added');
-          },
-    );
+	//read the url
+	//get things surrounded by {}
+	//get things beginning with :
+	//list out the keys followed by a text field and a save icon
+	//on save, write the k:v to the request query body
   }
 
   @override
