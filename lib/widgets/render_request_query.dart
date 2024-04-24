3da728b25227177e41dfeff108f38365cba584c5
Jack Leatherland <jpl92@live.co.uk>
2024-04-24 21:46:12 +0100

    set up pathvar taable

--- lib/widgets/render_request_query.dart
+++ lib/widgets/render_request_query.dart
@@ -23,23 +23,85 @@ class RenderRequestQuery extends StatelessWidget {
 
     return Column(
       children: [
-        ListView.builder(
-          itemCount: queryParams.length,
-          itemBuilder: (context, index){
-            return(Row(
-              children: [
-                TextFormField(initialValue: queryParams.keys.elementAt(index),),
-                TextFormField(initialValue: queryParams[queryParams.keys.elementAt(index)] as String)
-              ],
-            ));
-        })
+        const Flexible(
+            flex: 1,
+            child: Padding(
+              padding: EdgeInsets.only(top: 8.0),
+              child: Text('Query Parameters'),
+            )),
+        Flexible(
+          flex: 3,
+          child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: queryParams.length,
+              itemBuilder: (context, index) {
+                return (Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 8.0, right: 8.0, top: 4.0),
+                        child: TextFormField(
+                          initialValue: queryParams.keys.elementAt(index),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 8.0, right: 8.0, top: 4.0),
+                        child: TextFormField(
+                            initialValue:
+                                queryParams[queryParams.keys.elementAt(index)]
+                                    as String),
+                      ),
+                    )
+                  ],
+                ));
+              }),
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        const Flexible(
+            flex: 1,
+            child: Text('Path Variables')),
+        Flexible(
+          flex: 3,
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: pathVars.length,
+            itemBuilder: (context, index) {
+              return (Row(
+                children: [
+                  Expanded(
+                      child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 8.0, right: 8.0, top: 4.0),
+                    child: TextFormField(
+                      initialValue: pathVars.keys.elementAt(index),
+                    ),
+                  )),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 8.0, right: 8.0, top: 4.0),
+                      child: TextFormField(
+                          initialValue: pathVars[pathVars.keys.elementAt(index)]
+                              as String),
+                    ),
+                  )
+                ],
+              ));
+            },
+          ),
+        )
       ],
     );
   }
 
   @override
   Widget build(BuildContext context) {
-
     return renderQueryOptions();
   }
 }
