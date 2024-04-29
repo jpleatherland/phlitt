b984a7e9e45acc7ffaac057464fa01933c3f7797
Jack Leatherland <jpl92@live.co.uk>
2024-04-29 22:39:07 +0100

    todo, remove overlay on tap. make overlay transparent

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:qapic/model/collections_model.dart';
 import 'package:qapic/widgets/render_request_groups.dart';
 import 'package:qapic/pages/open_request.dart';
+import 'package:qapic/widgets/custom_context_menu_controller.dart';
 
 class MainPage extends StatefulWidget {
   final Collection collection;
@@ -33,62 +34,64 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
       } else {
         tabController.index = openRequests.indexOf(request);
       }
-
     }
 
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          title: Text(collection.collectionName,
-              style: TextStyle(color: colorScheme.onPrimary)),
-          actions: [
-            IconButton(
-                onPressed: () => writeback(),
-                icon: const Icon(Icons.save),
-                iconSize: 37,
-                color: colorScheme.onPrimary)
-          ]),
-      body: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Expanded(
-            flex: 1,
-            child: Column(children: [
-              RenderCollectionRequestGroups(
-                collection: collection,
-                selectRequest: selectRequest,
-              )
+    return GestureDetector(
+      onTap: () => CustomContextMenuController.removeAny(),
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            title: Text(collection.collectionName,
+                style: TextStyle(color: colorScheme.onPrimary)),
+            actions: [
+              IconButton(
+                  onPressed: () => writeback(),
+                  icon: const Icon(Icons.save),
+                  iconSize: 37,
+                  color: colorScheme.onPrimary)
             ]),
-          ),
-          Expanded(
-              flex: 5,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(2.0),
-                    child: TabBar(
-                        controller: tabController,
-                        tabs: openRequests
-                            .map(
-                              (e) => Tab(text: e.requestName),
-                            )
-                            .toList()),
-                  ),
-                  Expanded(
-                    child: TabBarView(
-                        controller: tabController,
-                        children: openRequests
-                            .map(
-                              (e) => Container(
-                                  padding: const EdgeInsets.all(5.0),
-                                  child: TabData(request: e)),
-                            )
-                            .toList()),
-                  ),
-                ],
-              )),
-        ],
+        body: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Column(children: [
+                RenderCollectionRequestGroups(
+                  collection: collection,
+                  selectRequest: selectRequest,
+                )
+              ]),
+            ),
+            Expanded(
+                flex: 5,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(2.0),
+                      child: TabBar(
+                          controller: tabController,
+                          tabs: openRequests
+                              .map(
+                                (e) => Tab(text: e.requestName),
+                              )
+                              .toList()),
+                    ),
+                    Expanded(
+                      child: TabBarView(
+                          controller: tabController,
+                          children: openRequests
+                              .map(
+                                (e) => Container(
+                                    padding: const EdgeInsets.all(5.0),
+                                    child: TabData(request: e)),
+                              )
+                              .toList()),
+                    ),
+                  ],
+                )),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/render_request_groups.dart
+++ lib/widgets/render_request_groups.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qapic/model/collections_model.dart';
+import 'package:qapic/widgets/custom_context_menu_controller.dart';
 
 class RenderCollectionRequestGroups extends StatelessWidget {
   final Collection collection;
@@ -7,6 +8,21 @@ class RenderCollectionRequestGroups extends StatelessWidget {
 
   const RenderCollectionRequestGroups(
       {super.key, required this.collection, required this.selectRequest});
+
+  void _showDialog(BuildContext context, Request request) {
+    Navigator.of(context).push(
+      DialogRoute<void>(
+        context: context,
+        builder: (BuildContext context) =>
+            Column(
+              children: [
+                const Text("Request Name:"),
+                // TextFormField(initialValue: request.requestName),
+              ],
+            ),
+      ),
+    );
+  }
 
   Widget renderCollectionRequestGroups(
       Collection collection, Function selectRequest) {
@@ -18,20 +34,51 @@ class RenderCollectionRequestGroups extends StatelessWidget {
           return ExpansionTile(
               title: Text(collection.requestGroups[index].requestGroupName),
               children: collection.requestGroups[index].requests
-                  .map((e) => TextButton.icon(
-                        style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.zero,
+                  .map((e) => _ContextMenuRegion(
+                        contextMenuBuilder:
+                            (BuildContext context, Offset offset) {
+                          return AdaptiveTextSelectionToolbar.buttonItems(
+                            anchors: TextSelectionToolbarAnchors(
+                              primaryAnchor: offset,
                             ),
+                            buttonItems: <ContextMenuButtonItem>[
+                              ContextMenuButtonItem(
+                                onPressed: () {
+                                  CustomContextMenuController.removeAny();
+                                  _showDialog(context, e);
+                                },
+                                label: 'Rename',
+                              ),
+                            ],
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 14.0),
+                          child: Row(
+                            children: [
+                              TextButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.zero,
+                                    ),
+                                  ),
+                                ),
+                                // dense: true,
+                                // trailing: const Icon(Icons.navigate_next),
+                                onPressed: () {
+                                  CustomContextMenuController.removeAny();
+                                  selectRequest(e);},
+                                child: Text(e.requestName),
+                              ),
+                              IconButton(
+                                  icon: const Icon(Icons.more_horiz),
+                                  onPressed: () {
+                                  })
+                            ],
                           ),
                         ),
-                        // dense: true,
-                        // trailing: const Icon(Icons.navigate_next),
-                        onPressed: () => selectRequest(e),
-                        icon: Text(e.requestName),
-                        label: const Icon(Icons.arrow_forward_ios, size: 14),
                       ))
                   .toList()
               // renderRequestNames(collection.requestGroups[index].requests),
@@ -42,5 +89,71 @@ class RenderCollectionRequestGroups extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return renderCollectionRequestGroups(collection, selectRequest);
+  }
+}
+
+typedef ContextMenuBuilder = Widget Function(
+    BuildContext context, Offset offset);
+
+class _ContextMenuRegion extends StatefulWidget {
+  /// Creates an instance of [_ContextMenuRegion].
+  const _ContextMenuRegion({
+    required this.child,
+    required this.contextMenuBuilder,
+  });
+
+  /// Builds the context menu.
+  final ContextMenuBuilder contextMenuBuilder;
+
+  /// The child widget that will be listened to for gestures.
+  final Widget child;
+
+  @override
+  State<_ContextMenuRegion> createState() => _ContextMenuRegionState();
+}
+
+class _ContextMenuRegionState extends State<_ContextMenuRegion> {
+  final CustomContextMenuController _contextMenuController = CustomContextMenuController();
+
+  void _onSecondaryTapUp(TapUpDetails details) {
+    _show(details.globalPosition);
+  }
+
+  void _onTap() {
+    if (!_contextMenuController.isShown) {
+      return;
+    }
+    _hide();
+  }
+
+  void _show(Offset position) {
+    _contextMenuController.show(
+      context: context,
+      contextMenuBuilder: (BuildContext context) {
+        return widget.contextMenuBuilder(context, position);
+      },
+    );
+  }
+
+  void _hide() {
+    _contextMenuController.remove();
+  }
+
+  @override
+  void dispose() {
+    _hide();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onSecondaryTapUp: _onSecondaryTapUp,
+      onTap: _onTap,
+      onLongPress: null,
+      onLongPressStart: null,
+      child: widget.child,
+    );
   }
 }
